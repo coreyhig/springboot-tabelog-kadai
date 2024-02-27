06fbccd16dc78e6f00adef9d0a91c4153758cd70
coreyhig <limpbizkithig@yahoo.co.jp>
2024-02-27 19:21:08 +0900

    first commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,13 @@
+ DROP TABLE IF EXISTS verification_tokens;
+ DROP TABLE IF EXISTS reservations;
+ DROP TABLE IF EXISTS store; 
+ 
  CREATE TABLE IF NOT EXISTS store (
      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(50) NOT NULL,  
      image_name VARCHAR(255),
      description VARCHAR(255) NOT NULL,
-     business_hours  VARCHAR(255) NOT NULL,
+     price INT NOT NULL,
      capacity INT NOT NULL,
      postal_code VARCHAR(50) NOT NULL,
      address VARCHAR(255) NOT NULL,
@@ -44,12 +48,12 @@
  
   CREATE TABLE IF NOT EXISTS reservations (
      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     Store_id INT NOT NULL,
+     store_id INT NOT NULL,
      user_id INT NOT NULL,
      checkin_date DATE NOT NULL,
      number_of_people INT NOT NULL,
      created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
      updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     FOREIGN KEY (Store_id) REFERENCES Store (id),
+     FOREIGN KEY (store_id) REFERENCES store (id),
      FOREIGN KEY (user_id) REFERENCES users (id)
  );
